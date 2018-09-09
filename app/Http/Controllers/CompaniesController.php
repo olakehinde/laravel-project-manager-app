6a21ccd17dde0adb99df39f68add51842a8ce7f3
olakehinde <olakehinde59@gmail.com>
2018-09-09 22:33:25 +0100

    added an edit function to the CompaniesControllers

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -61,7 +61,9 @@ class CompaniesController extends Controller
      */
     public function edit(Company $company)
     {
-        //
+        $company = Company::find($company->id);
+
+        return view('companies.edit', ['company' => $company]);
     }
 
     /**
